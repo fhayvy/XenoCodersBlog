6abb444018461c5c56d1c371bfb77cbaa3624bd6
VoN <hi@imrvon.com>
2022-09-26 15:15:10 +0100

    Create Static Files Structure

--- xenocoders/authentication/urls.py
+++ xenocoders/authentication/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('authentication.urls'))
+    path('', views.home),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- xenocoders/authentication/views.py
+++ xenocoders/authentication/views.py
@@ -1,3 +1,43 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib import messages
+from validate_email import validate_email
+from .models import User
 # Create your views here.
+
+def home(request):
+    return render(request, 'authentication/index.html')
+
+def login(request):
+    return render(request, 'authentication/login.html')
+
+def register(request):
+    if request.method == "POST":
+        context={'has_error': False}
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if len(password)<8:
+            messages.add_message(request,messages.ERROR, 'Password is less than 8 characters')
+            context['has_error'] = True
+        
+        if password != password2:
+            messages.add_message(request,messages.ERROR, 'Password mismatch')
+            context['has_error'] = True
+        
+        if not validate_email(email):
+            messages.add_message(request,messages.ERROR, 'Enter a valid email address')
+            context['has_error'] = True
+        
+        if not username:
+            messages.add_message(request,messages.ERROR, 'Username is required')
+            context['has_error'] = True
+        
+        if User.objects.filter(username=username).exists():
+            messages.add_message(request,messages.ERROR, 'Username is taken. Choose another one')
+            context['has_error'] = True
+
+    return render(request, 'authentication/register.html')
