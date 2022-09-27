31e9a76f4b62915ef04255c6c14a4e29e1e6430e
VoN <hi@imrvon.com>
2022-09-27 12:37:23 +0100

    Log In Done

--- xenocoders/authentication/urls.py
+++ xenocoders/authentication/urls.py
@@ -17,7 +17,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('login/', views.login, name='login'),
+    path('', views.home, name='home'),
+    path('login/', views.login_user, name='login'),
     path('register/', views.register, name='register'),
 ]
--- xenocoders/authentication/views.py
+++ xenocoders/authentication/views.py
@@ -1,43 +1,75 @@
+from audioop import reverse
 from multiprocessing import context
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from validate_email import validate_email
 from .models import User
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 # Create your views here.
 
 def home(request):
     return render(request, 'authentication/index.html')
 
-def login(request):
+def login_user(request):
+    if request.method == "POST":
+        context={'has_error': False, 'data':request.POST}
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username,password=password)
+
+        if not user:
+            messages.error(request, 'invalid credentials')
+            return render(request, 'authentication/login.html', context)
+        
+        login(request, user)
+        messages.success(request, f'Welcome {{user.username}}')
+
+        return redirect(reverse('home'))
+
     return render(request, 'authentication/login.html')
 
 def register(request):
     if request.method == "POST":
-        context={'has_error': False}
+        context={'has_error': False, 'data':request.POST}
         email = request.POST.get('email')
         username = request.POST.get('username')
         password = request.POST.get('password')
         password2 = request.POST.get('password2')
 
         if len(password)<8:
-            messages.add_message(request,messages.ERROR, 'Password is less than 8 characters')
+            messages.error(request, 'Password is less than 8 characters')
             context['has_error'] = True
         
         if password != password2:
-            messages.add_message(request,messages.ERROR, 'Password mismatch')
+            messages.error(request, 'Password mismatch')
             context['has_error'] = True
         
         if not validate_email(email):
-            messages.add_message(request,messages.ERROR, 'Enter a valid email address')
+            messages.error(request, 'Enter a valid email address')
             context['has_error'] = True
         
         if not username:
-            messages.add_message(request,messages.ERROR, 'Username is required')
+            messages.error(request, 'Username is required')
             context['has_error'] = True
         
         if User.objects.filter(username=username).exists():
-            messages.add_message(request,messages.ERROR, 'Username is taken. Choose another one')
+            messages.error(request, 'Username is taken. Choose another one')
             context['has_error'] = True
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'There already an account associated with this email.')
+            context['has_error'] = True
+        
+        if context['has_error']:
+            return render(request, 'authentication/register.html', context)
+        
+        user=User.objects.create_user(username=username, email=email)
+        user.set_password(password)
+        user.save()
+        messages.success(request, 'Account created. You can now Log In')
+
+        return redirect('login')
 
     return render(request, 'authentication/register.html')
