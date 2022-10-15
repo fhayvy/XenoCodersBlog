45d1fc7139ad501604c2df0ae1f7c93dfa5b01dc
mac <ezefavourchiamaka01@gmail.com>
2022-10-15 12:32:17 +0100

    added requirements.txt

--- xenocoders/xenocoders/settings.py
+++ xenocoders/xenocoders/settings.py
@@ -152,6 +152,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
